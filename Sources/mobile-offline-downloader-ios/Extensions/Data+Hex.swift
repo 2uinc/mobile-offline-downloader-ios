268632a786ae876cb788d35b2f313a7aa76c8bfd
Anton Yarmolenko <ayarmolenko@2u.com>
2023-06-27 15:12:16 +0200

    chore: added Data extension #2

--- Sources/mobile-offline-downloader-ios/Extensions/Data+Hex.swift
+++ Sources/mobile-offline-downloader-ios/Extensions/Data+Hex.swift
@@ -1,1 +1,8 @@
 import Foundation
+
+extension Data {
+    public var hexString: String {
+        let hexString = map { String(format: "%02.2hhx", $0) }.joined()
+        return hexString
+    }
+}
