534022871d56d6fb0137043231d5a0017959fd03
forgotvas <vadim@touchappmedia.com>
2023-08-01 16:19:51 +0300

    fix: favicon link

--- Sources/mobile-offline-downloader-ios/Utilites/OfflineLinkDownloader.swift
+++ Sources/mobile-offline-downloader-ios/Utilites/OfflineLinkDownloader.swift
@@ -10,7 +10,9 @@ public class OfflineLinkDownloader {
         progress.completedUnitCount = 0
         if Task.isCancelled { throw URLError(.cancelled) }
 
-        guard let url = URL(string: urlString) else {
+        let characterSet = CharacterSet(charactersIn: "{|}()'\"`@").inverted
+        guard let urlString = urlString.addingPercentEncoding(withAllowedCharacters: characterSet),
+              let url = URL(string: urlString) else {
             throw OfflineLinkDownloaderError.wrongURL(url: urlString)
         }
 
