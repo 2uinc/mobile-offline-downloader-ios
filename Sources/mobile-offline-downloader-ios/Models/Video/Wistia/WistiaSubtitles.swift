92be5187fcab655eba733480704fdbf02ffaae98
forgotvas <vadim@touchappmedia.com>
2023-08-09 19:02:51 +0300

    fix: wistia subtitles

--- Sources/mobile-offline-downloader-ios/Models/Video/Wistia/WistiaSubtitles.swift
+++ Sources/mobile-offline-downloader-ios/Models/Video/Wistia/WistiaSubtitles.swift
@@ -2,23 +2,20 @@ import Foundation
 
 struct WistiaSubtitles: Codable {
     let captions: [WistiaCaption]
-    let preferredLanguages: [String]
 
     enum CodingKeys: String, CodingKey {
         case captions
-        case preferredLanguages = "preferred_languages"
     }
 }
 
 struct WistiaCaption: Codable {
-    let id: Int
     let language, englishName, nativeName: String
     let rightToLeft: Bool
     let hash: WistiaHash?
     let key: String
 
     enum CodingKeys: String, CodingKey {
-        case id, language
+        case language
         case englishName = "english_name"
         case nativeName = "native_name"
         case rightToLeft = "right_to_left"
