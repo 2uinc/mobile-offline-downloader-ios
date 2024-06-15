e9f5c32a82f4a9b17351aefad4173c8613ecc0e5
Anton Yarmolenko <37253+rnr@users.noreply.github.com>
2024-06-15 11:22:47 +0200

    chore: set realm version exact 10.49.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
-        .package(url: "https://github.com/realm/realm-swift.git", from: "10.39.1")
+        .package(url: "https://github.com/realm/realm-swift.git", exact: "10.49.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
